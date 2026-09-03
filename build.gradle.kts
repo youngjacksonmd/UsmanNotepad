a91d7ee4b418c3c7cfe4226dbdcd3a6b47155061
youngjacksonmd <young.jackson@prgmd.com>
2026-09-03 04:20:55 -0500

    build: add Kotlin and Compose plugins for V3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
 plugins {
     id("com.android.application") version "8.9.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
 }
